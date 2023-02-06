15214858ba066960596127c6e780f36342c1eeb0
Thatcher Auten <thatcherauten24@gmail.com>
2023-02-06 12:31:38 -0600

    bugs

--- Loops.java
+++ Loops.java
@@ -1,10 +1,22 @@
 public class Loops {
 
-  // static methods go here
+  public static void nHellos(int n) {
+    int x = 1;
+    for (x = 1; x <= n; x++) {
+      if (x % 100 == 1) {
+        System.out.println(x + "st hello");
+      } else if (x % 10  == 2 || x % 100 == 2) {
+        System.out.println(x + "nd hello");
+      } else if (x % 10  == 3 || x % 100 == 3) {
+        System.out.println(x + "rd hello");
+      } else {
+        System.out.println(x + "th hello");
+      }
+    }
+
+  }
 
   public static void main(String[] args) {
-
-    // testing of static methods goes here
-
+    nHellos(222);
   }
 }
